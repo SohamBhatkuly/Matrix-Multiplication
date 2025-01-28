68a608146dc39836df65050492441f720aa7a9d5
Soham <sohambhatkuly@gmail.com>
2025-01-28 23:58:15 +0530

    matrix generation

--- matrix.js
+++ matrix.js
@@ -3,6 +3,19 @@ form.addEventListener('input', (event) => {
     event.preventDefault();
     let rows = document.getElementById('rows').value;
     let columns = document.getElementById('columns').value;
-
     console.log(rows, columns);
+    let root = document.createElement('div');
+    root.setAttribute('id', 'root');
+        root.setAttribute('class', 'root');
+    for (let i = 0; i < rows; i++){
+        let rowEntry = document.createElement('div');
+        rowEntry.setAttribute('id', `${i}`);
+        for (let j = 0; j < columns; j++){
+        let colEntry = document.createElement('div');
+            colEntry.setAttribute('id', `${j}`);
+            rowEntry.appendChild(colEntry);
+        }
+        root.appendChild(rowEntry);
+    }
+    document.body.appendChild(root);
 })
