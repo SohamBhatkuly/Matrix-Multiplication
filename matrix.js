da33ec23f56206883a5049f1c52d33ec973291f2
Soham <sohambhatkuly@gmail.com>
2025-01-29 18:09:07 +0530

    grid changes

--- matrix.js
+++ matrix.js
@@ -15,18 +15,32 @@ form.addEventListener('input', (event) => {
         root.innerHTML = ``;
         root.setAttribute('class', 'root');
     }
-    root.style.display = "grid";
-    root.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
-    root.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+
+    if (rows && columns) {
+        root.style.display = "grid";
+        root.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+        root.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+        root.style.columnGap = `1px`;
+        root.style.width = "max-content";
+        root.style.maxWidth = "900px";
+        root.style.border = "1px solid black";
+        root.style.borderRadius = "0.5rem";
+        root.style.padding = "0.2rem";
+    }
+
     // root.style.border= "1px solid black"
+   
 
     for (let i = 0; i < rows; i++){
         // let rowEntry = document.createElement('div');
         // rowEntry.setAttribute('id', `${i}`);
         for (let j = 0; j < columns; j++){
         let colEntry = document.createElement('div');
-            colEntry.setAttribute('id', `${j}`);
-            colEntry.innerHTML=`<input type="number"></input>`
+            colEntry.setAttribute('id', `${i+j}`);
+            colEntry.innerHTML = `<input type="number" class="input-values"></input>`
+            colEntry.style.border = "1px solid black";
+            colEntry.style.margin = '2px';
+            colEntry.style.borderRadius = "0.1rem"
             root.appendChild(colEntry);
         }
         // root.appendChild(rowEntry);
